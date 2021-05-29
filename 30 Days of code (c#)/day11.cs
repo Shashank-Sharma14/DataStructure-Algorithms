71f3f91dde3367e3aec778b044de1270efea118b
Shashank Sharma <60505766+Shashank-Sharma14@users.noreply.github.com>
2021-05-29 15:55:04 +0530

    Add files via upload

---
+++ 30 Days of code (c#)/day11.cs
@@ -0,0 +1,42 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+        int count = 0, max = -2147483648;
+        for (int i = 0; i < arr.Count - 2; i++)
+        {
+            for (int j = 0; j < arr[i].Count - 2; j++)
+            {
+                count = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+                max = Math.Max(max, count);
+            }
+        }
+
+        Console.WriteLine(max);
+
+    }
+}
