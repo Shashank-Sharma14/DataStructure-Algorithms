95c527a5a3c0417e4b805dc6e103d686d2b8687e
Shashank Sharma <60505766+Shashank-Sharma14@users.noreply.github.com>
2021-08-08 21:55:34 +0530

    Update lowerboundnumber.cpp

--- Array/lowerboundnumber.cpp
+++ Array/lowerboundnumber.cpp
@@ -4,51 +4,32 @@ using namespace std;
 
 int lowerBound(vector<int> A, int Val) {
 
-    // your code goes here
-
     int s = 0;
-
     int e = A.size() - 1;
-
 
     while (s <= e) {
 
         int mid = (s + e) / 2;
 
         if (A[mid] == Val) {
-
             return A[mid];
-
         }
-
         else if (A[mid] < Val) {
-
             s = mid + 1;
-
         }
-
         else {
-
             e = mid - 1;
-
         }
-
     }
-   
-
+ 
     if (A[s] > Val && s==0) {
 
-        return A[e - 1];// when all elements are greater than Val
-
+        return -1; // when all elements are greater than Val
     }
    else {
 
         return A[e];
-
-    }
-
-    
-
+    }   
 }
 int main() {
     // your code goes here
